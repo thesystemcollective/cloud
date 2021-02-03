1f22ccac2fe653925a4004ee4802c55ac7637757
jaeh <jascha@jaeh.at>
2021-02-03 18:33:26 +0100

    bootstrap/init: cd into docker dir fixed

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -17,13 +17,11 @@ printf "\033[1;33mTheSystemCloud\033[0m - cloning thesystemcollective/cloud - "
 DIR="~/cloud"
 if [ ! -d "$DIR" ] ; then
   git clone --depth 1 git://github.com/thesystemcollective/cloud $DIR # >> /var/log/TheSystemCloud-install.log 2>&1
+  cd "$DIR"
 else
   cd "$DIR"
   git pull origin master # >> /var/log/TheSystemCloud-install.log 2>&1
 fi
-
-
-cd cloud
 
 printf "done \n\n"
 

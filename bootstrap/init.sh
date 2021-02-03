d44c97890fc83762d47931c6238e35643ac9c567
jaeh <jascha@jaeh.at>
2021-02-03 19:11:04 +0100

    format

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -15,6 +15,7 @@ printf "done \n\n"
 printf "\033[1;33mTheSystemCloud\033[0m - cloning thesystemcollective/cloud - "
 
 DIR="~/cloud"
+
 if [ ! -d "$DIR" ] ; then
   git clone --depth 1 git://github.com/thesystemcollective/cloud $DIR # >> /var/log/TheSystemCloud-install.log 2>&1
   cd "$DIR"

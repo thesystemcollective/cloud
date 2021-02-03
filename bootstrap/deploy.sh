80c2318574fa97dafad52438d44dc9800b0ebbd8
jaeh <jascha@jaeh.at>
2021-02-03 20:18:41 +0100

    deploy: more robust, add tasks

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -7,16 +7,40 @@ printf "\033[1;33mTheSystemCloud\033[0m - pushing to production.\n\n"
 source ./bootstrap/functions.sh
 
 file_exists .secrets/ssh.env
-file_exists .secrets/db.env
 file_exists bootstrap/init.sh
+file_exists bootstrap/services.sh
 
 source .secrets/ssh.env
 
 var_exists $USER \$USER
 var_exists $IP \$IP
 
-echo "using ssh user: $USER and ip: $IP"
+printf "\033[1;33mTheSystemCloud\033[0m - using ssh user: $USER and ip: $IP\n\n"
 
-scp .secrets/db.env $USER@$IP:db.env
+if [[ $@ == *"init"* ]]; then
+  printf "\033[1;33mTheSystemCloud\033[0m - install dependencies"
+  
+  file_exists .secrets/db.env
 
-ssh $USER@$IP bash -s < bootstrap/init.sh
+  scp .secrets/db.env $USER@$IP:db.env
+
+  scp .secrets/hetzner.ini $USER@$IP:hetzner.ini
+
+  ssh $USER@$IP bash -s < bootstrap/init.sh
+
+  printf " - done\n\n"
+fi
+
+if [[ $@ == *"services"* ]]; then
+  printf "\033[1;33mTheSystemCloud\033[0m - install dependencies"
+  
+  ssh $USER@$IP bash -s < bootstrap/services.sh
+
+  printf " - done\n\n"
+fi
+
+printf "\033[1;33mTheSystemCloud\033[0m - cleanup"
+  
+ssh $USER@$IP rm -f db.env hetzner.ini
+
+printf " - done\n\n"
